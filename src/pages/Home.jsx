be80bfc58ee0366f1e828bd81f99edc918614fac
wintermute28 <a28c@yandex.ru>
2024-11-06 15:47:38 +0300

    Add search in categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,53 @@
 import { useState, useEffect } from "react";
-
+import { useLocation, useNavigate } from "react-router-dom";
 import { getAllCategories } from "../api";
 import Preloader from "../components/Preloader";
 import CaterogyList from "../components/CaterogyList";
+import Search from "../components/Search";
 
 const Home = () => {
   const [catalog, setCatalog] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const { pathname, search } = useLocation();
+  const navigate = useNavigate();
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+    navigate({
+      pathname,
+      search: `?search=${str}`,
+    });
+  };
 
   useEffect(() => {
     getAllCategories().then((data) => {
       setCatalog(data.categories);
+      setFilteredCatalog(
+        search
+          ? data.categories.filter((item) =>
+              item.strCategory
+                .toLowerCase()
+                .includes(search.split("=")[1].toLowerCase())
+            )
+          : data.categories
+      );
     });
-  }, []);
+  }, [search]);
 
   return (
     <>
+      <Search cb={handleSearch} />
       <h4 style={{ textAlign: "center" }}>Categories</h4>
-      {catalog.length ? <CaterogyList catalog={catalog} /> : <Preloader />}
+      {catalog.length ? (
+        <CaterogyList catalog={filteredCatalog} />
+      ) : (
+        <Preloader />
+      )}
     </>
   );
 };
